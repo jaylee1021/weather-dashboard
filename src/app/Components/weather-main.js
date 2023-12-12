c45382cf7b52403a04cfd85338256b140e06b6bb
Jay Lee <leejayjong@gmail.com>
2023-12-12 15:46:36 -0800

    second commit

---
+++ src/app/Components/weather-main.js
@@ -0,0 +1,14 @@
+'use client';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+export default function WeatherMain() {
+
+
+    
+    return (
+        <>
+            this is weather main
+        </>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import './css/page.css';
+import WeatherMain from './Components/weather-main';
 
 export default function Home() {
   return (
-    <> </>
+    <main className='main'>
+      <WeatherMain />
+    </main>
   );
 }
