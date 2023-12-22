26cf92761ea51806cf136be2b753375d97a61a93
Jay Lee <leejayjong@gmail.com>
2023-12-22 12:00:36 -0800

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import './css/page.css';
 import WeatherMain from './Components/weather-main';
+import Weather from './Components/weather';
 
 export default function Home() {
   return (
     <main className='main'>
+      {/* <Weather /> */}
       <WeatherMain />
     </main>
   );
