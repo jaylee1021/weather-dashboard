8990137f7a2be39893053254c9e164ce36fe18f8
Jay Lee <leejayjong@gmail.com>
2023-12-28 15:23:00 -0800

    feat: add more options, remove axios call

--- src/app/Components/CustomCharts.js
+++ src/app/Components/CustomCharts.js
@@ -10,9 +10,9 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 
-export default function CustomCharts() {
-    const [loading, setLoading] = useState(true);
-    const [weatherData, setWeatherData] = useState([]);
+export default function CustomCharts({ weatherData }) {
+
+    // const [weatherData, setWeatherData] = useState([]);
     const [dataLabel, setDataLabel] = useState('');
     const [currentHour, setCurrentHour] = useState('');
     const [data, setData] = useState([
@@ -24,44 +24,45 @@ export default function CustomCharts() {
         hAxis: { title: 'Time', titleTextStyle: { color: '#333' } },
         vAxis: { minValue: 0 },
         height: 300,
-        legend: 'none',
+        legend: { position: "top", maxLines: 3 },
         chartArea: { width: "90%", height: "70%" }
     };
     const mphToKnots = 0.868976;
+    const mphToMetersPerSec = 0.44704;
 
     function handleDataUpdate(e) {
         setDataLabel(e.target.value);
         const weatherDataLength = weatherData.length - currentHour < 7 ? weatherData.length : 7;
-        if (e.target.value === 'steadyWinds') {
-            const newData = [['Time', 'Wind Speed (knots)']];
-            for (let i = (currentHour - 3); i < weatherDataLength; i++) {
-                newData.push([weatherData[i].time.split(' ')[1], weatherData[i].wind_mph * mphToKnots]);
-                setData(newData);
+        if (e.target.value === 'winds_knots') {
+            const newData = [['Time', 'Wind Speed (knots)', 'Wind Gust (knots)']];
+            for (let i = (currentHour - 3); i < currentHour + weatherDataLength; i++) {
+                newData.push([weatherData[i].time.split(' ')[1], weatherData[i].wind_mph * mphToKnots, weatherData[i].gust_mph * mphToKnots]);
             }
-        } else if (e.target.value === 'windGust') {
-            const newData = [['Time', 'Wind Gust (knots)']];
-            for (let i = (currentHour - 3); i < weatherDataLength; i++) {
-                newData.push([weatherData[i].time.split(' ')[1], weatherData[i].gust_mph * mphToKnots]);
-                setData(newData);
+            setData(newData);
+        } else if (e.target.value === 'winds_m/s') {
+            const newData = [['Time', 'Wind Speed (m/s)', 'Wind Gust (m/s)']];
+            for (let i = (currentHour - 3); i < currentHour + weatherDataLength; i++) {
+                newData.push([weatherData[i].time.split(' ')[1], weatherData[i].wind_mph * mphToMetersPerSec, weatherData[i].gust_mph * mphToMetersPerSec]);
             }
+            setData(newData);
         } else if (e.target.value === 'temp_f') {
             const newData = [['Time', 'Air Temp (f)']];
-            for (let i = (currentHour - 3); i < weatherDataLength; i++) {
+            for (let i = (currentHour - 3); i < currentHour + weatherDataLength; i++) {
                 newData.push([weatherData[i].time.split(' ')[1], weatherData[i].temp_f]);
-                setData(newData);
             }
+            setData(newData);
         } else if (e.target.value === 'precip_mm') {
             const newData = [['Time', 'Precipitation (mm/hr)']];
-            for (let i = (currentHour - 3); i < weatherDataLength; i++) {
+            for (let i = (currentHour - 3); i < currentHour + weatherDataLength; i++) {
                 newData.push([weatherData[i].time.split(' ')[1], weatherData[i].precip_mm]);
-                setData(newData);
             }
+            setData(newData);
         } else if (e.target.value === 'vis_miles') {
             const newData = [['Time', 'Visibility (SM)']];
-            for (let i = (currentHour - 3); i < weatherDataLength; i++) {
+            for (let i = (currentHour - 3); i < currentHour + weatherDataLength; i++) {
                 newData.push([weatherData[i].time.split(' ')[1], weatherData[i].vis_miles]);
-                setData(newData);
             }
+            setData(newData);
         }
     }
 
@@ -75,38 +76,6 @@ export default function CustomCharts() {
         return () => clearInterval(intervalId); // This is the cleanup function
     }, []);
 
-
-    useEffect(() => {
-        const fetchData = () => {
-            axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=47.53294,-121.80539&days=2`)
-                .then((res) => {
-                    console.log('weather data', res.data.forecast.forecastday[0].hour);
-                    setWeatherData(res.data.forecast.forecastday[0].hour);
-                    setLoading(false);
-
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-            // setMinCountdown(60);
-        };
-        fetchData();
-
-        // const intervalId = setInterval(fetchData, 60000);
-        // const countdownInterval = setInterval(() => {
-        //     setMinCountdown(prevCountdown => prevCountdown > 0 ? prevCountdown - 1 : 0);
-        // }, 1000);
-
-        // return () => {
-        //     clearInterval(intervalId); // This is the cleanup function
-        //     clearInterval(countdownInterval);
-        // };
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <div className='py-10 flex flex-col items-center justify-center'>
             <div>
@@ -119,10 +88,10 @@ export default function CustomCharts() {
                         onChange={handleDataUpdate}
                         label="Weather Data"
                     >
-                        <MenuItem value={'steadyWinds'}>Steady Winds (knots)</MenuItem>
-                        <MenuItem value={'windGust'}>Wind Gust (knots)</MenuItem>
+                        <MenuItem value={'winds_knots'}>Winds (knots)</MenuItem>
+                        <MenuItem value={'winds_m/s'}>Winds (m/s)</MenuItem>
                         <MenuItem value={'temp_f'}>Air Temp (f)</MenuItem>
-                        <MenuItem value={'precip_mm'}>Previpitation (mm/hr)</MenuItem>
+                        <MenuItem value={'precip_mm'}>Precipitation (mm/hr)</MenuItem>
                         <MenuItem value={'vis_miles'}>Visibility (SM)</MenuItem>
                     </Select>
                 </FormControl>
