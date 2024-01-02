8f2fb48053909d018ad9e8e01159ce16dbe3249f
Jay Lee <randomdori@gmail.com>
2024-01-02 09:43:46 -0800

    feat: add title

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import WeatherMain from './Components/weather-main';
 export default function Home() {
   return (
     <main className='main'>
+      <h1 style={{ color: 'black' }}> Flight Test Weather Dashboard</h1>
       <WeatherMain />
     </main>
   );
