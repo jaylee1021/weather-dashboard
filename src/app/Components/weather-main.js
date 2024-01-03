6643483eb50b89e64e6aaa59e804780084a87a83
Jay Lee <randomdori@gmail.com>
2024-01-02 20:05:39 -0800

    chore: remove testing console.log

--- src/app/Components/weather-main.js
+++ src/app/Components/weather-main.js
@@ -130,7 +130,6 @@ export default function WeatherMain() {
 
             selectedSite = process.env.NEXT_PUBLIC_PDT10_NORTH_PAD_COORDINATES;
         }
-        console.log('fetching data');
         axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${selectedSite}`)
             .then((res) => {
                 setForecast(res.data.forecast.forecastday[0].hour);
