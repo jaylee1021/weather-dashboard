280f842bb52d713e0343058c6ef22d48100fe53b
Jay Lee <leejayjong@gmail.com>
2024-01-09 11:06:00 -0800

    chore: move title to weather-main

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import WeatherMain from './Components/weather-main';
 export default function Home() {
   return (
     <main className='main'>
-      <h1 className='page_title'> Flight Test Weather Dashboard</h1>
       <WeatherMain />
     </main>
   );
