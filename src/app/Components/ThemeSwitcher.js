f22c7ef80332d653d134ab6b208552fe82167484
Jay Lee <leejayjong@gmail.com>
2024-01-18 15:20:25 -0800

    feat: add dark team switch

---
+++ src/app/Components/ThemeSwitcher.js
@@ -0,0 +1,31 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { FormGroup, FormControlLabel, Switch } from "@mui/material";
+
+
+const ThemeSwitcher = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
+
+    return (
+        <FormGroup>
+            <FormControlLabel
+                control={<Switch checked={theme === 'dark'}
+                    onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')} />}
+                label="Dark Mode"
+            />
+            {/* <div>
+                The current theme is: {theme}
+                <button onClick={() => setTheme('light')}>Light Mode</button>
+                <button onClick={() => setTheme('dark')}>Dark Mode</button>
+            </div> */}
+        </FormGroup>
+    );
+};
+
+export default ThemeSwitcher;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './css/globals.css';
+import Providers from './Components/providers';
+import ThemeSwitcher from './Components/ThemeSwitcher';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Providers>
+          {/* <ThemeSwitcher /> */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
